413f2202bb17f2551e1a0f06f848860355245a48
pgomesdev <gomes.pedro15@gmail.com>
2019-01-30 08:06:26 -0300

    ui: added styling to the NewDeck component

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { Text, TextInput, TouchableOpacity, StyleSheet, KeyboardAvoidingView } from 'react-native'
 import { connect } from 'react-redux'
 import { handleAddDeck } from '../actions'
 
@@ -15,29 +15,79 @@ class NewDeck extends Component {
   }
 
   onPress = () => {
+    if (!this.state.title) {
+      return alert('The deck must have a title')
+    }
+
     this.props.dispatch(handleAddDeck(this.state.title))
 
     this.setState(() => ({
       title: '',
     }))
+
+    alert('Deck created!')
   }
 
   render() {
     return (
-      <View>
-        <Text>Insert the title of your new Deck</Text>
+      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+        <Text style={styles.title}>Insert the title of your new Deck</Text>
         <TextInput
+          style={styles.input}
           onChangeText={this.onChange}
           value={this.state.title}
         />
         <TouchableOpacity
+          style={styles.submitButton}
           onPress={this.onPress}
         >
-          <Text>Create Deck</Text>
+          <Text style={styles.submitButtonText}>Create Deck</Text>
         </TouchableOpacity>
-      </View>
+      </KeyboardAvoidingView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    width: '80%',
+    height: 40,
+    margin: 10,
+    padding: 10,
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  },
+  submitButton: {
+    backgroundColor: 'black',
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  submitButtonText: {
+    color: 'white',
+    fontSize: 18,
+  },
+})
 
 export default connect()(NewDeck)
